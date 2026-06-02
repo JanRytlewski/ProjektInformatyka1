b3a3f60b2869b73672a2cb2c914b1148072dda71
JanRytlewski <rytlewski.jan2010@gmail.com>
2026-06-02 12:33:04 +0200

    rozpoczecie programu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+using namespace std;
+int t[7][7];
 
 int main() {
-    std::cout << "hello world" << std::endl;
+    for (int i = 0; i < 7; i++) {
+        for(int j = 0; j < 7; j++) {
+            t[i][j] = 0;
+            cout<<t[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
     return 0;
 }
 
